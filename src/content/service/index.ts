db12298748c767ab6cb5833bd9d4452fe5879db7
wyk <wangyukun@haxibiao.com>
2020-08-13 18:27:16 +0800

    完善content模块，添加operation

--- src/content/service/index.ts
+++ src/content/service/index.ts
@@ -1,5 +1,7 @@
 export { GQL } from './graphql';
+export { errorMessage } from './errorMessage';
 export { exceptionCapture } from './exceptionCapture';
 export { useBetterMutation } from './useBetterMutation';
 export { useLikeMutation } from './useLikeMutation';
 export { useDeleteMutation } from './useDeleteMutation';
+export { useReport } from './useReport';
