80e9780a42f5fe9a082efd8f20961f065ee8779d
wyk <wangyukun@haxibiao.com>
2020-09-02 20:13:42 +0800

    修改AppConfig，简化配置信息

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,22 +1,15 @@
 import { Platform } from 'react-native';
-// import DeviceInfo from 'react-native-device-info';
 import Config from 'react-native-config';
+// import DeviceInfo from 'react-native-device-info';
 import {
-    name as Name,
     AppID,
     DisplayName as AppName,
-    AppSlogan,
+    name as PackageName,
+    Version,
     Build,
+    AppSlogan,
     ServerRoot,
     UploadServer,
-    Version,
-    AppVersionNumber,
-    AndroidOnline,
-    iOSOnline,
-    goldAlias,
-    energyAlias,
-    qqGroup,
-    appStoreUrl,
     WechatAppId,
 } from '@app/app.json';
 
@@ -27,22 +20,18 @@ const AppStore = Config && Config.APP_STORE ? Config.APP_STORE : 'haxibiao'; // 
 // AppVersionNumber = parseFloat(AppVersionNumber.join('')); //app vesrsion 数值
 
 export default {
-    ServerRoot,
-    UploadServer,
-    Name,
-    AppName,
-    AppVersion: Version,
     AppID,
-    AppSlogan,
+    AppName,
+    PackageName,
     Version,
     Build,
-    AppStore,
-    AppVersionNumber,
-    AndroidOnline,
-    iOSOnline,
-    goldAlias: goldAlias || '金币',
-    energyAlias: energyAlias || '精力',
-    qqGroup: qqGroup || '1036821462',
-    iosAppStoreUrl: appStoreUrl || 'https://apps.apple.com/cn/app/id1526799985',
+    AppSlogan,
+    ServerRoot,
+    UploadServer,
     WechatAppId,
+    AppStore,
+    goldAlias: '金币',
+    ticketAlias: '精力',
+    qqGroup: '1036821462',
+    iosAppStoreUrl: 'https://apps.apple.com/cn/app/id1526799985',
 };
