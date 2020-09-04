6d291ffcc3816941bbb31130c51669e7fecd27ee
wyk <wangyukun@haxibiao.com>
2020-09-04 16:27:02 +0800

    YXSP-70 优化好评界面，完成好评任务

--- src/screens/task/Praise.tsx
+++ src/screens/task/Praise.tsx
@@ -9,158 +9,210 @@ import {
     Keyboard,
     Linking,
     TextInput,
-    Button,
 } from 'react-native';
-import { PageContainer, TouchFeedback, Iconfont, MediaUploader, ItemSeparator } from '@src/components';
+import { Iconfont, MediaUploader, HxfButton, DropdownMenu, NavBarHeader } from '@src/components';
+import { BoxShadow } from 'react-native-shadow';
+import { GQL, useQuery, useMutation, useApolloClient, errorMessage } from '@src/apollo';
+import { appStore, userStore } from '@src/store';
 
-import ModalDropdown from './components/ModalDropdown';
-import { GQL, useQuery, useMutation, useApolloClient } from '@src/apollo';
-import { appStore } from '@src/store';
-import { locatedError } from 'graphql';
+const shadowOpt = {
+    width: Device.WIDTH,
+    color: '#E8E8E8',
+    border: 10,
+    // radius: 10,
+    opacity: 0.4,
+    x: 0,
+    y: 10,
+    style: {
+        marginTop: 0,
+    },
+};
 
-const { width, height } = Dimensions.get('window');
+const appStores = ['OPPO', 'VIVO', '小米', '华为', '魅族', '百度', '豌豆荚', '应用宝'];
 
-export default props => {
-    const [formData, setFormData] = useState({ content: '', images: [] });
-    const uploadResponse = useCallback(response => {
-        setFormData(prevFormData => {
-            return { ...prevFormData, images: response };
-        });
+export default (props) => {
+    const [appStore, setAppStore] = useState('');
+    const [phone, setPhoneNumber] = useState('');
+    const [images, setImages] = useState([]);
+    const uploadResponse = useCallback((response) => {
+        setImages(response);
     }, []);
-    const [phone, onChangeNumber] = useState('');
-
-    const user = props.route.params?.user || {};
-    console.log('user.id', user);
-    console.log('Token', TOKEN);
 
     const client = useApolloClient();
 
-    const highPraiseTask = () => {
+    const highPraiseTask = useCallback(() => {
         client
             .mutate({
                 mutation: GQL.highPraiseTaskMutation,
-                variables: { id: 19, content: phone },
+                variables: { user_id: userStore.me?.id, info: appStore, account: phone, images },
                 refetchQueries: () => [
                     {
                         query: GQL.tasksQuery,
                     },
                 ],
             })
-            .then(result => {
-                Toast.show({ content: '好评通过审核后，奖励会自动发放，可在账单明细中查看' });
+            .then((result) => {
+                Toast.show({ content: '提交成功，请关注奖励发放' });
                 props.navigation.goBack();
             })
-            .catch(err => {
-                console.log('err', err, client);
+            .catch((err) => {
+                Toast.show({ content: errorMessage(err) || '提交失败' });
             });
-    };
-
-    const appstores = ['OPPO', 'VIVO', '小米', '华为', '魅族', '百度', '豌豆荚', '应用宝'];
+    }, [appStore, phone, images]);
 
     return (
-        <PageContainer title="好评任务">
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={{ fontSize: font(15) }}>去应用商店好评</Text>
-                    <TouchFeedback
-                        style={styles.row}
-                        onPress={() =>
-                            Linking.openURL(Device.IOS ? Config.iosAppStoreUrl : 'market://details?id=' + Config.AppID)
-                        }>
-                        <Text style={{ fontSize: font(12), color: Theme.grey }}>去应用商店评价</Text>
-                        <Iconfont name="right" size={pixel(14)} color={Theme.grey} />
-                    </TouchFeedback>
-                </View>
-
-                <View
-                    style={{
-                        paddingVertical: pixel(4),
-                        borderBottomWidth: pixel(0.5),
-                        borderColor: Theme.borderColor,
-                    }}>
-                    <TextInput
-                        // style={styles.input}
-                        placeholder={'应用商店账号(手机号)'}
-                        maxLength={11}
-                        onChangeText={onChangeNumber}
-                        keyboardType="numeric"
-                    />
-                </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: pixel(15) }}>
-                    <Text style={{ marginVertical: pixel(10) }}>应用商店</Text>
-                    <ModalDropdown
-                        defaultValue={'请选择应用商店'}
-                        options={appstores} //下拉内容数组
-                        showsVerticalScrollIndicator={false}
-                        style={{
-                            textAlign: 'center',
-                            borderRadius: 100,
-                            backgroundColor: '#F6DB4A55',
-                            paddingVertical: pixel(10),
-                            paddingHorizontal: pixel(10),
-                            // marginHorizontal: pixel(15),
-                        }} //按钮样式
-                        dropdownStyle={{
-                            height: 32 * 4.8,
-                            marginRight: -pixel(10),
-                            alignItems: 'center',
-                        }} //下拉框样式
-                        dropdownTextStyle={{
-                            backgroundColor: '#F6DB4A55',
-                            justifyContent: 'center',
-                            borderRadius: 20,
-                            alignItems: 'center',
-                        }}
-                    />
-                </View>
-
-                <View style={[styles.formTitleWrap, { marginVertical: pixel(15) }]}>
-                    <Text style={{ fontSize: font(16), color: Theme.primaryFont }}>
-                        好评内容截图 ({formData.images.length}/3)
-                    </Text>
-                </View>
-                <View style={styles.mediaWrap}>
-                    <MediaUploader
-                        type="image"
-                        maximum={3}
-                        onResponse={uploadResponse}
-                        maxWidth={Device.WIDTH / 2}
-                        style={styles.mediaItem}
-                    />
-                </View>
-                <View style={styles.button}>
-                    <Button title={'提交'} disabled={!(phone && formData.images.length > 0)} onPress={highPraiseTask} />
-                </View>
+        <View style={styles.container}>
+            <NavBarHeader title="应用商店好评" statusbarProperties={{ barStyle: 'dark-content' }} />
+            <ScrollView contentContainerStyle={styles.contentContainer} keyboardShouldPersistTaps={'always'}>
+                <TouchableOpacity
+                    style={styles.operationItem}
+                    onPress={() =>
+                        Linking.openURL(Device.IOS ? Config.iosAppStoreUrl : 'market://details?id=' + Config.AppID)
+                    }>
+                    <Text style={styles.operationName}>应用商店好评</Text>
+                    <View style={styles.operationRight}>
+                        <Text style={{ fontSize: font(14), color: '#b2b2b2' }}>去应用商店评价</Text>
+                        <Iconfont name="right" size={font(14)} color={'#b2b2b2'} />
+                    </View>
+                </TouchableOpacity>
+                <DropdownMenu
+                    style={{ height: pixel(500) }}
+                    bgColor={'white'}
+                    dropStyle={styles.operationItem}
+                    dropItemStyle={{ alignItems: 'flex-end' }}
+                    labels={[appStore || '请选择应用商店']}
+                    label={<Text style={styles.operationName}>应用商店</Text>}
+                    tintColor={appStore ? Theme.primaryColor : '#b2b2b2'}
+                    activityTintColor={Theme.primaryColor}
+                    handler={(selection, row) => setAppStore(appStores[row])}
+                    data={appStores}>
+                    <View style={styles.appStoreContent}>
+                        <View style={styles.operationItem}>
+                            <Text style={styles.operationName}>应用商店账号</Text>
+                            <TextInput
+                                style={styles.inputStyle}
+                                textAlignVertical={'top'}
+                                placeholder={'请输入应用商店账号(手机号)'}
+                                maxLength={11}
+                                defaultValue={phone}
+                                onChangeText={(value) => {
+                                    setPhoneNumber(value);
+                                }}
+                            />
+                        </View>
+                        <View style={styles.operationItem}>
+                            <Text style={styles.operationName}>评论内容截图</Text>
+                            <Text style={{ fontSize: font(14), color: '#b2b2b2' }}>（{images.length}/3）</Text>
+                        </View>
+                    </View>
+                    <View style={styles.mediaContainer}>
+                        <MediaUploader
+                            type="image"
+                            maximum={3}
+                            onResponse={uploadResponse}
+                            maxWidth={Device.WIDTH / 2}
+                            style={styles.mediaItem}
+                        />
+                    </View>
+                    <View style={styles.ruleContainer}>
+                        <Text style={styles.ruleTitle}>温馨提示</Text>
+                        <Text style={styles.ruleText}>
+                            在应用商店给我们评价后并截图，同您在应用商店的账号（手机号）一并提交。
+                        </Text>
+                        <Text style={styles.ruleText}>后续请留意审核状态，经审核通过后即可领取奖励。</Text>
+                    </View>
+                </DropdownMenu>
+            </ScrollView>
+            <View style={styles.buttonWrap}>
+                <HxfButton
+                    gradient={true}
+                    disabled={!(phone && appStore && images.length > 0)}
+                    style={styles.buttonStyle}
+                    title={'提交'}
+                    onPress={highPraiseTask}
+                />
             </View>
-        </PageContainer>
+        </View>
     );
 };
+
+const MediaItemWidth = (Device.WIDTH - pixel(60)) / 3;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Theme.white || '#FFF',
-        marginHorizontal: pixel(10),
-        padding: pixel(Theme.itemSpace),
+        backgroundColor: '#FFF',
     },
-    header: {
+    contentContainer: {
+        flexGrow: 1,
+        paddingHorizontal: pixel(12),
+        paddingBottom: Theme.HOME_INDICATOR_HEIGHT + pixel(80),
+    },
+    operationItem: {
+        height: pixel(56),
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        height: 60,
-        backgroundColor: '#fff',
+        borderBottomWidth: pixel(1),
+        borderBottomColor: '#f0f0f0',
     },
-    row: {
+    operationName: {
+        fontSize: font(16),
+        color: '#2b2b2b',
+    },
+    operationRight: {
         flexDirection: 'row',
         alignItems: 'center',
     },
-
-    button: {
-        marginTop: pixel(40),
-        marginHorizontal: pixel(7),
+    appStoreContent: {
+        marginBottom: pixel(20),
+    },
+    inputStyle: {
+        textAlign: 'right',
+        alignSelf: 'stretch',
+        marginLeft: pixel(15),
+        height: pixel(50),
+        margin: 0,
+        padding: 0,
+        fontSize: font(14),
+        paddingVertical: pixel(15),
+    },
+    mediaContainer: {
+        marginRight: -pixel(15),
+        marginBottom: pixel(10),
+    },
+    mediaItem: {
+        width: MediaItemWidth,
+        height: MediaItemWidth,
+        marginRight: pixel(15),
+    },
+    ruleContainer: {
+        marginTop: pixel(10),
+    },
+    ruleTitle: {
+        color: '#2b2b2b',
+        fontSize: font(14),
+        fontWeight: 'bold',
+    },
+    ruleText: {
+        color: '#969696',
+        fontSize: font(14),
+        lineHeight: font(18),
+        paddingVertical: pixel(5),
+    },
+    buttonWrap: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        padding: pixel(15),
+        paddingBottom: Theme.HOME_INDICATOR_HEIGHT + pixel(30),
+        backgroundColor: '#fff',
+    },
+    buttonStyle: {
+        height: pixel(44),
         borderRadius: pixel(5),
+        alignItems: 'center',
         justifyContent: 'center',
-        height: 40,
-        width: width * 0.82,
     },
 });
