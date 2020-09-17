0f05a150ca7f0d800d6db5e5413ead9acb93711a
PBK-Bin <bin@PBK6.cn>
2020-09-17 14:08:19 +0800

    YXSP-86 修复微信分享回调闪退问题

--- android/app/src/main/java/com/yinxiangshipin/MainApplication.java
+++ android/app/src/main/java/com/yinxiangshipin/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.microsoft.codepush.react.CodePush;
 
+import com.theweflex.react.WeChatPackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -34,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new WeChatPackage());
           return packages;
         }
 
