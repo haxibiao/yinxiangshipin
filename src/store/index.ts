0f627e7a5e04a8898a11128da7a92a0986add70c
wyk <wangyukun@haxibiao.com>
2020-10-15 18:10:38 +0800

    修复mobx batching waring

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Keys, Storage } from './localStorage';
+// import 'mobx-react-lite/batchingOptOut';
 export { Keys, Storage };
 
 // FIXME context模式和mobx observer并存有风险,需要剔除context
 export default React.createContext({});
 
 export { when } from 'mobx';
-export { observer, useObserver } from 'mobx-react-lite';
+export { observer } from 'mobx-react-lite';
 export { observable } from 'mobx';
 
 export { default as appStore } from './appStore';
