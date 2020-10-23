96fd44d4f4c3f97fbbe246e86c2433975185d978
wyk <wangyukun@haxibiao.com>
2020-10-23 09:23:46 +0800

    导出account gql

---
+++ src/apollo/graphgqls/account/index.ts
@@ -0,0 +1,21 @@
+// 用户账号
+
+import autoSignInMutation from './autoSignInMutation.graphql';
+import destroyUserMutation from './destroyUserMutation.graphql';
+import otherSignInMutation from './otherSignInMutation.graphql';
+import retrievePasswordMutation from './retrievePasswordMutation.graphql';
+import signInMutation from './signInMutation.graphql';
+import signUpMutation from './signUpMutation.graphql';
+import smsSignInMutation from './smsSignInMutation.graphql';
+import updateUserInfoSecurity from './updateUserInfoSecurity.graphql';
+
+export const account = {
+    autoSignInMutation,
+    destroyUserMutation,
+    otherSignInMutation,
+    retrievePasswordMutation,
+    signInMutation,
+    signUpMutation,
+    smsSignInMutation,
+    updateUserInfoSecurity,
+};
--- src/apollo/graphgqls/index.ts
+++ src/apollo/graphgqls/index.ts
@@ -6,12 +6,12 @@ import { favorite } from './favorite';
 import { feedback } from './feedback';
 import { follow } from './follow';
 import { like } from './like';
-import { live } from './live';
 import { notification } from './notification';
 import { post } from './post';
 import { report } from './report';
 import { reward } from './reward';
 import { search } from './search';
+import { share } from './share';
 import { tag } from './tag';
 import { task } from './task';
 import { user } from './user';
@@ -26,12 +26,12 @@ export const GQL = {
     ...feedback,
     ...follow,
     ...like,
-    ...live,
     ...notification,
     ...post,
     ...report,
     ...reward,
     ...search,
+    ...share,
     ...tag,
     ...task,
     ...user,
