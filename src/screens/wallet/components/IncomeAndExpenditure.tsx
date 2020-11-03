d2f54d99ac47e648682febf80e55d2823f0d7a14
张旭 <zhangxu@zhangxudeMac-mini.local>
2020-11-03 10:45:24 +0800

    我的账单=>明细重构

---
+++ src/screens/wallet/components/IncomeAndExpenditure.tsx
@@ -0,0 +1,33 @@
+/*
+ * @flow
+ * created by wyk made in 2019-04-11 17:14:30
+ */
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
+import { GQL } from '@src/apollo';
+import { QueryList } from '@src/content';
+import { userStore } from '@src/store';
+import IncomeAndExpenditureItem from './IncomeAndExpenditureItem';
+
+export default function IntegralDetail(props: any) {
+    const { me: user } = userStore;
+    const renderItem = useCallback(({ item, index, data, page }) => {
+        return <IncomeAndExpenditureItem item={item} />;
+    }, []);
+    return (
+        <View style={{ flex: 1 }}>
+            <QueryList
+                gqlDocument={GQL.goldsHistoryQuery}
+                dataOptionChain="golds.data"
+                options={{
+                    variables: {
+                        user_id: user.id,
+                    },
+                    fetchPolicy: 'network-only',
+                }}
+                renderItem={renderItem}
+                showsVerticalScrollIndicator={false}
+            />
+        </View>
+    );
+}
---
+++ src/screens/wallet/components/IncomeAndExpenditureItem.tsx
@@ -0,0 +1,45 @@
+/*
+ * @flow
+ * created by wyk made in 2019-04-11 18:10:13
+ */
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Row, SafeText } from '@src/components';
+
+export default function IncomeAndExpenditureItem(props: any) {
+    const { item } = props;
+    return (
+        <View style={styles.item}>
+            <Row style={{ justifyContent: 'space-between' }}>
+                <SafeText style={{ fontSize: font(15), color: Theme.defaultTextColor }}>{item.remark}</SafeText>
+                <SafeText
+                    style={{
+                        fontSize: font(20),
+                        color: item.gold > 0 ? Theme.primaryColor : Theme.secondaryColor,
+                    }}>
+                    {item.gold > 0 ? '+' + item.gold : item.gold}
+                </SafeText>
+            </Row>
+            <Row style={{ justifyContent: 'space-between', marginTop: pixel(10) }}>
+                <SafeText style={{ fontSize: font(12), color: Theme.subTextColor }}>{item.created_at}</SafeText>
+                <SafeText
+                    style={{
+                        fontSize: font(12),
+                        color: Theme.subTextColor,
+                    }}>
+                    {`剩余${Config.goldAlias}: ${item.balance}`}
+                </SafeText>
+            </Row>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    item: {
+        padding: pixel(Theme.itemSpace),
+        borderBottomColor: Theme.borderColor,
+        borderBottomWidth: pixel(1),
+    },
+});
