1bd812d41100b87dafab21924b0b66446de3e9b8
张旭 <zhangxu@zhangxudeMac-mini.local>
2020-11-03 14:03:45 +0800

    新的粉丝Query=>QueryList

--- src/screens/notification/FollowScreen.tsx
+++ src/screens/notification/FollowScreen.tsx
@@ -1,63 +1,36 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
-import { PageContainer, StatusView, SpinnerLoading, UserItem } from '@src/components';
-import { Query, GQL, useApolloClient } from '@src/apollo';
+import React, { useEffect, useCallback } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { PageContainer, UserItem } from '@src/components';
+import { GQL, useApolloClient } from '@src/apollo';
 import { useNavigation } from '@react-navigation/native';
+import { QueryList } from '@src/content';
 
 export default (props: any) => {
     const client = useApolloClient();
     const navigation = useNavigation();
-
     useEffect(() => {
         return () => {
             client.query({
                 query: GQL.unreadsQuery,
                 fetchPolicy: 'network-only',
             });
-
-            console.log('out');
         };
+    }, []);
+    const renderItem = useCallback((item, index) => {
+        return <UserItem user={item.user} navigation={navigation} />;
     }, []);
     return (
         <PageContainer title="新的粉丝">
             <View style={styles.container}>
-                <Query query={GQL.followersNotificationsQuery} variables={{ fetchPolicy: 'network-only' }}>
-                    {({ loading, error, data, refetch, fetchMore }) => {
-                        const items = data?.notifications?.data;
-                        if (error) return <StatusView.ErrorView onPress={refetch} error={error} />;
-                        if (!items && loading) return <SpinnerLoading />;
-                        if (items.length <= 0) return <StatusView.EmptyView />;
-                        const { hasMorePages, currentPage } = data?.notifications?.paginatorInfo;
-                        return (
-                            <FlatList
-                                data={items}
-                                renderItem={({ item }) => <UserItem user={item.user} navigation={navigation} />}
-                                onEndReached={() => {
-                                    if (hasMorePages) {
-                                        fetchMore({
-                                            variables: {
-                                                page: currentPage + 1,
-                                            },
-                                            updateQuery: (prev, { fetchMoreResult }) => {
-                                                if (fetchMoreResult && fetchMoreResult.notifications) {
-                                                    return Object.assign({}, prev, {
-                                                        notifications: Object.assign({}, prev.notifications, {
-                                                            paginatorInfo: fetchMoreResult.notifications.paginatorInfo,
-                                                            data: [
-                                                                ...prev.notifications.data,
-                                                                ...fetchMoreResult.notifications.data,
-                                                            ],
-                                                        }),
-                                                    });
-                                                }
-                                            },
-                                        });
-                                    }
-                                }}
-                            />
-                        );
+                <QueryList
+                    gqlDocument={GQL.followersNotificationsQuery}
+                    dataOptionChain="notifications.data"
+                    paginateOptionChain="notifications.paginatorInfo"
+                    options={{
+                        fetchPolicy: 'network-only',
                     }}
-                </Query>
+                    renderItem={renderItem}
+                />
             </View>
         </PageContainer>
     );
